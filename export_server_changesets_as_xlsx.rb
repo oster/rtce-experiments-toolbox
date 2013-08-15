8ac552a58e4eef27bd5bfa16cefe52d8cbd00b13
Gerald Oster <oster@loria.fr>
2013-08-15 23:06:33 +0200

    scripts to export changes as .xls

--- export_server_changesets_as_xlsx.rb
+++ export_server_changesets_as_xlsx.rb
@@ -14,15 +14,14 @@ data = Delays.values()
 # end
 
 
-workbook = WriteXLSX.new('changesets-client.xlsx')
+workbook = WriteXLSX.new('changesets-server.xlsx')
 header_format = workbook.add_format(:bold => 1)
 raw_timestamp_format = workbook.add_format
 raw_timestamp_format.set_num_format(' 0') # number
 fmt_timestamp_format = workbook.add_format
 #fmt_timestamp_format.set_num_format('[h]:mm:ss,000') # [h]:mm:ss,000
 
-#[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 16, 17, 18, 19, 20, 21, 25].each do |group|
-[5, 6, 7, 8, 9, 10, 12, 13, 14, 16, 17, 18, 19, 20, 21, 25].each do |group|
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 16, 17, 18, 19, 20, 21, 25].each do |group|
   [:corrections, :films].each do |experiment|
             
     if group == 14 && experiment == :films then num = 15 else num = group end  # to fix group number for bad experimentation
@@ -37,20 +36,14 @@ fmt_timestamp_format = workbook.add_format
     worksheet.set_column(2, 2, nil, fmt_timestamp_format)
     worksheet.set_row(0, nil, header_format)
     
-    delays = computeTimeDelays("DATA-by-num/#{num}/etherpad.log.gz", padid)
-
-    # NOTE: works only for experiments from 006 to 010
-    css = loadChangeSets("DATA-by-num/#{num}/dirtyCS.db.gz", padid)
-    resynchTimestamps(css, delays)
-        
-    #css = loadChangeSetsFromServerDB("DATA-by-num/#{num}/dirty.db.gz", padid)
+    css = loadChangeSetsFromServerDB("DATA-by-num/#{num}/dirty.db.gz", padid)
     mints = css.select { |cs| cs.timestamp? }.min_by { |cs| cs.timestamp }.timestamp
     
       
     row = 0
     col = 0
-    #["userid", "timestamp", "formatted-timestamp"].each do |v|
-    ["userid", "timestamp", "formatted-timestamp", "event-type", "position", "line", "column", "value", "inserted", "deleted"].each do |v|
+    ["userid", "timestamp", "formatted-timestamp"].each do |v|
+    #["userid", "timestamp", "formatted-timestamp", "event-type", "position", "line", "column", "value", "inserted", "deleted"].each do |v|
       worksheet.write(row, col, v)
       col += 1
     end
@@ -58,8 +51,8 @@ fmt_timestamp_format = workbook.add_format
     row = 1
     css.each do |cs| 
       col = 0
-      #[cs.userid, cs.timestamp, format_timestamp(cs.timestamp, mints) ].each do |v|
-      [cs.userid, cs.timestamp, format_timestamp(cs.timestamp, mints), cs.type, cs.position, cs.line, cs.column, cs.value.nil? ? 0 : cs.value.size, cs.inserted, cs.deleted].each do |v|
+      [cs.userid, cs.timestamp, format_timestamp(cs.timestamp, mints) ].each do |v|
+      #[cs.userid, cs.timestamp, format_timestamp(cs.timestamp, mints), cs.type, cs.position, cs.line, cs.column, cs.value.nil? ? 0 : cs.value.size, cs.inserted, cs.deleted].each do |v|
         worksheet.write(row, col, v)
         col += 1  
       end
