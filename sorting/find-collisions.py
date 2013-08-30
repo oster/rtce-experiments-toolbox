15053dfe573d3579b1bf5b8c5af91e0d90aedc5e
Gerald Oster <oster@loria.fr>
2013-08-30 13:25:24 +0200

    batch processing for finding collisions in sorting experiments

--- sorting/find-collisions.py
+++ sorting/find-collisions.py
@@ -1,9 +1,11 @@
 #! /usr/bin/python -tt
 
+import os
 import re
 import pprint
 import json
 import gzip
+import datetime
 
 from ranking import *
 
@@ -34,9 +36,6 @@ def load_pad_revisions(db_file, padid):
 			except StopIteration:
 				break
 		return revisions
-	
-pp = pprint.PrettyPrinter(indent=4)
-
 
 last_uid = -1
 user_ids = {}
@@ -52,109 +51,104 @@ def userId(str_uid):
 		user_ids[str_uid] = last_uid
 		return last_uid
 
+def find_collisions(revs, standard_titles):
+	collisions = {}
+	
+	for rev in revs:
+		target_titles = rev['content']
+		for title in standard_titles:
+			count = target_titles.count(title)
+	
+			if not title in collisions:
+				collisions[title] = {}
+				collisions[title]['count'] = 0
+				collisions[title]['history'] = []
+	
+			if count != collisions[title]['count']:				
+				op = {}
+				op['rev'] = rev['rev']
+				timestamp = datetime.datetime.fromtimestamp(int(rev['timestamp']) / 1000)
+				op['timestamp'] = timestamp.strftime('%Y-%m-%d %H:%M:%S') #datetime.datetime(2005, 7, 14, 12, 30)
+				op['time-in-second'] = timestamp.hour * 3600 + timestamp.minute * 60 + timestamp.second
+				op['user'] = userId(rev['author'])
+				op['type'] = count - collisions[title]['count']				
+				collisions[title]['history'].append(op)
+				collisions[title]['count'] = count
+	  
+	# backup-ing history
+	for title in collisions:
+		collisions[title]['life'] = collisions[title]['history'][:]
+	
+	# removing initial insertion
+	for title in collisions:
+		for op in collisions[title]['history'][:]:
+			if op['rev'] == 1 and op['type'] == 1:
+				collisions[title]['history'].remove(op) # beware! dangerous!
+				
+	# removing cut-and-paste
+	for title in collisions:
+		prev_op = None
+		for op in collisions[title]['history'][:]:
+			if not prev_op is None:
+				if prev_op['type'] == -1 and op['type'] == +1 and prev_op['user'] == op['user']:
+					collisions[title]['history'].remove(prev_op)
+					collisions[title]['history'].remove(op)				
+					prev_op = None
+				else:
+					prev_op = op
+			else:
+				prev_op = op
+	
+	# removing copy-paste-cut
+	for title in collisions:
+		prev_op = None
+		for op in collisions[title]['history'][:]:
+			if not prev_op is None:
+				if prev_op['type'] == 1 and op['type'] == -1 and prev_op['user'] == op['user']:
+					collisions[title]['history'].remove(prev_op)
+					collisions[title]['history'].remove(op)
+					prev_op = None
+				else:
+					prev_op = op
+			else:
+				prev_op = op
+	
+	# cleaning - removing title with empty collision history
+	cleaned_collisions = {}
+	for title in collisions:
+		if len(collisions[title]['history']) > 0:
+			cleaned_collisions[title] = collisions[title]
+	collisions = cleaned_collisions
+	del cleaned_collisions
+	
+	return collisions
 
-revs = load_pad_revisions('../DATA-by-num/010/dirty.db.gz', 'films006')
 
+
+pp = pprint.PrettyPrinter(indent=4)
+
+INPUT_DATA_PATH='../DATA-by-num/'
+OUTPUT_DATA_PATH='./DATA-collisions/'
 standard_titles = titles(read_movies('final.txt'))
 
-#target_titles = revs[1]['content'] #.splitlines()
+for group in ('001', '002', '003', '004', '005', '006', '007', '008', '009', '010', '012', '013', '014', '016', '017', '018', '019', '020', '021', '025'):
+	if group == '014':
+		num = '015'
+	else:
+		num = group
+	
+	revisions = load_pad_revisions(INPUT_DATA_PATH + group + '/dirty.db.gz', 'films' + num)
+	collisions = find_collisions(revisions, standard_titles)
 
-collisions = {}
+	f = open(OUTPUT_DATA_PATH + 'films' + num + '-collisions.txt', 'w')
+	f.write(pp.pformat(collisions))
+	f.close()
 
-for rev in revs:
-	target_titles = rev['content']
-	missing = []
-	duplicates = []
-	for title in standard_titles:
-		count = target_titles.count(title)
 
-		if not title in collisions:
-			collisions[title] = {}
-			collisions[title]['count'] = 0
-			collisions[title]['history'] = []
 
-		if count != collisions[title]['count']:
-			
-			op = {}
-			op['rev'] = rev['rev']
-			op['timestamp'] = rev['timestamp']
-			op['user'] = userId(rev['author'])
-			op['type'] = count - collisions[title]['count']
-			
-			#collisions[title]['history'].append( (rev['rev'], rev['timestamp'], rev['author'], count - collisions[title]['count']) )
-			collisions[title]['history'].append(op)
-			collisions[title]['count'] = count
-  
-# backup-ing history
-for title in collisions:
-	collisions[title]['life'] = collisions[title]['history'][:]
 
-# removing initial insertion
-for title in collisions:
-	for op in collisions[title]['history']:
-		if op['rev'] == 1 and op['type'] == 1:
-			collisions[title]['history'].remove(op) # beware! dangerous!
-			
-# removing cut-and-paste
-for title in collisions:
-	toberemoved = []
-	prev_op = None
-	for op in collisions[title]['history']:
-		if not prev_op is None:
-			if prev_op['type'] == -1 and op['type'] == +1 and prev_op['user'] == op['user']:
-				#collisions[title]['history'].remove(prev_op)
-				#collisions[title]['history'].remove(op)				
-				toberemoved.append(prev_op)
-				toberemoved.append(op)
-				prev_op = None
-			else:
-				prev_op = op
-		else:
-			prev_op = op
-	for dop in toberemoved: 
-		collisions[title]['history'].remove(dop)
 
-# removing copy-paste-cut
-for title in collisions:
-	toberemoved = []	
-	prev_op = None
-	for op in collisions[title]['history']:
-		if not prev_op is None:
-			if prev_op['type'] == 1 and op['type'] == -1 and prev_op['user'] == op['user']:
-				#collisions[title]['history'].remove(prev_op)
-				#collisions[title]['history'].remove(op)
-				toberemoved.append(prev_op)
-				toberemoved.append(op)
-				prev_op = None
-			else:
-				prev_op = op
-		else:
-			prev_op = op
-	for dop in toberemoved: 
-		collisions[title]['history'].remove(dop)	
 
-# cleaning - removing title with empty collision history
-cleaned_collisions = {}
-for title in collisions:
-	if len(collisions[title]['history']) > 0:
-		cleaned_collisions[title] = collisions[title]
-collisions = cleaned_collisions
-del cleaned_collisions
 
-# 
-# 
-# #		if count == 0:
-# #		#if not title in target_titles:
-# #			missing.append(title)
-# #		elif count > 1:
-# #			duplicates.append(title)
-# #			
-# #			
-# #	print 'Missing movies in rev %i:' % ( rev['rev'] )
-# #	print pp.pformat(missing)
-# #	print 'Duplicated movies in rev %i:' % ( rev['rev'] )
-# #	print pp.pformat(duplicates)
-
-print pp.pformat(collisions)
 
 
